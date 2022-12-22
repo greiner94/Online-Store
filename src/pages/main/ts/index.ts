ccc5e86fae09bb1b2495043900a2aff22cb883d5
gena <genagreiner94@gmail.com>
2022-12-22 13:07:37 +0200

    feat: add values to sidebar checkbox filters

--- src/pages/main/ts/index.ts
+++ src/pages/main/ts/index.ts
@@ -1,3 +1,13 @@
 import '../index.html';
 import '../style/style.scss';
 import '../../../assets/ts/template';
+
+import categoryFilter from './categoryFilter';
+import brandFilter from './brandFilter';
+import priceFilter from './priceFilter';
+import stockFilter from './stockFilter';
+
+categoryFilter();
+brandFilter();
+priceFilter();
+stockFilter();
