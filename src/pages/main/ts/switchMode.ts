e7bfeb88769b4b0637f5bbd0ae128a3965ad82de
dairinka <104695300+dairinka@users.noreply.github.com>
2022-12-25 16:16:55 +0200

    feat: add load mode according query params

--- src/pages/main/ts/switchMode.ts
+++ src/pages/main/ts/switchMode.ts
@@ -1,17 +1,20 @@
+import setQueryParams from './setQueryParams';
 const modeCards = <HTMLElement>document.querySelector('.view-mode__cards');
 const modeList = <HTMLElement>document.querySelector('.view-mode__list');
 const productBlock = <HTMLElement>document.querySelector('.card-block');
-function switchMode(event: Event): void {
-    
-    const target = <HTMLElement>event.target;
-    if (target.className === 'view-mode__cards') {
+
+export function switchMode<T>(data: T): void {
+    console.log('data', data);
+    const target = <HTMLElement>(<Event>data).target || null;
+    if (target?.className === 'view-mode__cards' || data === 'cards') {
         modeCards.classList.add('active');
         modeList.classList.remove('active');
         productBlock.classList.remove('list-block');
-    } else {
+        setQueryParams('mode', 'cards');
+    } else if (target?.className === 'view-mode__list' || data === 'list') {
         modeCards.classList.remove('active');
         modeList.classList.add('active');
         productBlock.classList.add('list-block');
+        setQueryParams('mode', 'list');
     }
 }
-export default switchMode;
