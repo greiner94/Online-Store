166817ad019ef3312af0f8d1316f5b4865ce702a
dairinka <104695300+dairinka@users.noreply.github.com>
2022-12-25 16:15:49 +0200

    feat: page loading according to query params

---
+++ src/pages/main/ts/windowOnload.ts
@@ -0,0 +1,25 @@
+import { switchMode } from './switchMode';
+
+/**
+ * Need change. Add parameters for load page
+ */
+export function windowOnload() {
+    document.addEventListener('DOMContentLoaded', () => {
+        changePageWithQueryParams();
+    });
+}
+
+function changePageWithQueryParams(): void {
+    const url = new URL(window.location.href);
+    const localProp = 'query';
+    const queryParams = JSON.parse(<string>localStorage.getItem(localProp));
+    for (const key in queryParams) {
+        url.searchParams.set(key, queryParams[key]);
+        // Add parameter to this place: if (key === 'brand') than call your function
+        if (key === 'mode') {
+            const value = <string>queryParams[key];
+            switchMode<string>(value);
+        }
+    }
+    window.history.pushState({}, '', url);
+}
