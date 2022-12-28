b2829bc86d020c32a0afcd30ed87825454bd65ba
gena <genagreiner94@gmail.com>
2022-12-28 22:33:00 +0200

    feat: add price filter

---
+++ src/pages/main/ts/priceFilter/watchByCards.ts
@@ -0,0 +1,44 @@
+import renderProuductsCards from '../renderProuductsCards';
+
+interface productData {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+}
+
+function watchByCards() {
+    const cardsWrapper = document.querySelector('.card-block') as HTMLElement;
+    const [inputStart, inputEnd] = document.querySelectorAll(
+        '.sidebar__filter-price input'
+    ) as NodeListOf<HTMLInputElement>;
+    const observer = new MutationObserver(() => {
+        const showedProductCards: productData[] = JSON.parse(localStorage.getItem('productCards') || '');
+        const filterCardsByPrice = showedProductCards.sort((a, b) => a.price - b.price);
+        const minFilterPrice = filterCardsByPrice[0].price.toString();
+        const maxFilterPrice = filterCardsByPrice[filterCardsByPrice.length - 1].price.toString();
+
+        inputStart.value = minFilterPrice;
+        inputEnd.value = maxFilterPrice;
+
+        const beginPriceElement = document.querySelector('.price-amount .amount-begin') as HTMLElement;
+        const endPriceElement = document.querySelector('.price-amount .amount-end') as HTMLElement;
+
+        beginPriceElement.textContent = minFilterPrice;
+        endPriceElement.textContent = maxFilterPrice;
+    });
+    observer.observe(cardsWrapper, {
+        childList: true,
+        subtree: true,
+        characterDataOldValue: true,
+    });
+}
+
+export default watchByCards;
