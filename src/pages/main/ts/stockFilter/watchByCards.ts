6fddab353144996d7d0a72c7a38652e77b870cde
gena <genagreiner94@gmail.com>
2022-12-30 23:22:59 +0200

    fix: show zero to range filter when show no cards

--- src/pages/main/ts/stockFilter/watchByCards.ts
+++ src/pages/main/ts/stockFilter/watchByCards.ts
@@ -19,18 +19,25 @@ function watchByCards() {
     ) as NodeListOf<HTMLInputElement>;
     const observer = new MutationObserver(() => {
         const showedProductCards: productData[] = JSON.parse(localStorage.getItem('productCards') || '');
-        const filterCardsByStock = showedProductCards.sort((a, b) => a.stock - b.stock);
-        const minFilterStock = filterCardsByStock[0].stock.toString();
-        const maxFilterStock = filterCardsByStock[filterCardsByStock.length - 1].stock.toString();
-
-        inputStart.value = minFilterStock;
-        inputEnd.value = maxFilterStock;
-
         const beginStockElement = document.querySelector('.stock-amount .amount-begin') as HTMLElement;
         const endStockElement = document.querySelector('.stock-amount .amount-end') as HTMLElement;
+        if (showedProductCards.length != 0) {
+            const filterCardsByStock = showedProductCards.sort((a, b) => a.stock - b.stock);
+            const minFilterStock = filterCardsByStock[0].stock.toString();
+            const maxFilterStock = filterCardsByStock[filterCardsByStock.length - 1].stock.toString();
 
-        beginStockElement.textContent = minFilterStock;
-        endStockElement.textContent = maxFilterStock;
+            inputStart.value = minFilterStock;
+            inputEnd.value = maxFilterStock;
+
+            beginStockElement.textContent = minFilterStock;
+            endStockElement.textContent = maxFilterStock;
+        } else {
+            inputStart.value = '0';
+            inputEnd.value = '0';
+
+            beginStockElement.textContent = '0';
+            endStockElement.textContent = '0';
+        }
     });
     observer.observe(cardsWrapper, {
         childList: true,
