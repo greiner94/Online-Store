2391743be9a0b6c9c2afbe724ce506d459cc89b3
dairinka <104695300+dairinka@users.noreply.github.com>
2023-01-01 00:18:19 +0200

    feat: add cart pagination

--- src/pages/main/ts/setLocalStorageParams.ts
+++ src/pages/main/ts/setLocalStorageParams.ts
@@ -1,10 +1,12 @@
 import { CartData } from './type';
 import { showEmptyCart } from './cartDisplay';
+import { setQueryParams } from './setQueryParams';
 export function setCartDataToLocalStorage(newCartData: CartData[]): void {
     const cartDataWithoutZeroAmount = newCartData.filter(({ amount }) => amount !== 0);
     const newCartDataJson: string = JSON.stringify(cartDataWithoutZeroAmount);
     localStorage.setItem('cart', newCartDataJson);
 }
+
 export function setCartAmountToLocalStorage(newAmount: number): void {
     const wrapper = <HTMLElement>document.querySelector('.product-cart__wrapper');
     const localProp = 'all-amount';
@@ -15,4 +17,16 @@ export function setCartAmountToLocalStorage(newAmount: number): void {
         localStorage.removeItem(localProp);
         showEmptyCart(wrapper);
     }
+}
+
+export function setAmountProductsOnPageToLocalstorage(newAmountProducts: number): void {
+    const localProp = 'cart-page';
+    const newAmountProductsJson: string = JSON.stringify(newAmountProducts);
+    localStorage.setItem(localProp, newAmountProductsJson);
+}
+
+export function setNumberOfPageToLocalStorage(page: number): void {
+    const localProp = 'page-number';
+    localStorage.setItem(localProp, page.toString());
+    setQueryParams('page', page.toString());
 }
